6a0fa28d62e14d3fa67a33ee085cfeb407cee128
Coda Hale <coda.hale@gmail.com>
2009-03-02 17:40:35 -0800

    Ripped out ProxyAdapter's guts and replaced it with something golden.

--- src/main/java/com/wesabe/bouncer/ProxyAdapter.java
+++ src/main/java/com/wesabe/bouncer/ProxyAdapter.java
@@ -1,112 +1,39 @@
 package com.wesabe.bouncer;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
-import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 
 import com.sun.grizzly.tcp.http11.GrizzlyAdapter;
 import com.sun.grizzly.tcp.http11.GrizzlyRequest;
 import com.sun.grizzly.tcp.http11.GrizzlyResponse;
+import com.wesabe.bouncer.http.ProxyRequest;
+import com.wesabe.bouncer.http.ProxyRequestFactory;
+import com.wesabe.bouncer.http.ProxyResponse;
+import com.wesabe.bouncer.http.ProxyResponseFactory;
 
 public class ProxyAdapter extends GrizzlyAdapter {
 	private static final Logger LOGGER = Logger.getLogger(ProxyAdapter.class.getName());
 	private final BackendService backendService;
-	private final HttpHeaders httpHeaders;
+	private final ProxyRequestFactory requestFactory;
+	private final ProxyResponseFactory responseFactory;
 
-	public ProxyAdapter(BackendService backendService, HttpHeaders httpHeaders) {
+	public ProxyAdapter(BackendService backendService, ProxyRequestFactory requestFactory,
+			ProxyResponseFactory responseFactory) {
 		super();
 		this.backendService = backendService;
-		this.httpHeaders = httpHeaders;
+		this.requestFactory = requestFactory;
+		this.responseFactory = responseFactory;
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void service(GrizzlyRequest request, GrizzlyResponse response) {
 		try {
-			buildResponse(backendService.execute(buildProxyRequest(request)), response);
+			ProxyRequest proxyRequest = requestFactory.buildFromGrizzlyRequest(request);
+			ProxyResponse proxyResponse = backendService.execute(proxyRequest);
+			responseFactory.buildFromHttpResponse(proxyResponse, response);
 		} catch (Exception e) {
 			LOGGER.log(Level.SEVERE, "Unhandled internal error", e);
 		}
-	}
-
-	@SuppressWarnings("unchecked")
-	private void buildResponse(HttpResponse proxyResponse, GrizzlyResponse response) throws IOException {
-		// set status
-		response.setStatus(proxyResponse.getStatusLine().getStatusCode());
-		
-		// set headers
-		for (Header header : proxyResponse.getAllHeaders()) {
-			if (httpHeaders.isValidResponseHeader(header.getName())) {
-				response.addHeader(header.getName(), header.getValue());
-			}
-		}
-		
-		// always overwrite the Server field
-		response.setHeader("Server", "Wesabe");
-		
-		// set entity, if any
-		if (proxyResponse.getEntity() != null) {
-			copyStream(proxyResponse.getEntity().getContent(), response.getOutputStream());
-		}
-		
-		// send
-		response.finishResponse();
-	}
-
-	private HttpUriRequest buildProxyRequest(GrizzlyRequest request) {
-		final String uri = request.getRequestURI();
-		final HttpUriRequest proxyRequest;
-		
-		if (request.getMethod().equals("GET")) {
-			proxyRequest = new HttpGet(uri);
-		} else {
-			// FIXME coda@wesabe.com -- Feb 27, 2009: buildRequest should handle DELETE, POST, PUT, OPTIONS, and a few others
-			throw new IllegalArgumentException("No idea how to handle a " + request.getMethod() + " request");
-		}
-		
-		Enumeration<?> headerNames = request.getHeaderNames();
-		while (headerNames.hasMoreElements()) {
-			String headerName = (String) headerNames.nextElement();
-			if (httpHeaders.isValidRequestHeader(headerName)) {
-				proxyRequest.addHeader(headerName, request.getHeader(headerName));
-			}
-		}
-		
-		proxyRequest.setHeader("X-Forwarded-For", request.getRemoteAddr());
-		
-		return proxyRequest;
-	}
-	
-	private void copyStream(InputStream input, OutputStream output) throws IOException {
-		final ReadableByteChannel inputChannel = Channels.newChannel(input);
-		final WritableByteChannel outputChannel = Channels.newChannel(output);
-		final ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
-		
-		while (inputChannel.read(buffer) != -1) {
-			buffer.flip();
-			outputChannel.write(buffer);
-			buffer.compact();
-		}
-		
-		buffer.flip();
-		
-		while (buffer.hasRemaining()) {
-			outputChannel.write(buffer);
-		}
-
-		inputChannel.close();
-		outputChannel.close();
 	}
 }
--- src/test/java/com/wesabe/bouncer/tests/ProxyAdapterTest.java
+++ src/test/java/com/wesabe/bouncer/tests/ProxyAdapterTest.java
@@ -1,178 +1,76 @@
 package com.wesabe.bouncer.tests;
 
-import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.message.BasicHeader;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatcher;
-import org.mockito.InOrder;
 
-import com.sun.grizzly.tcp.Request;
-import com.sun.grizzly.tcp.Response;
-import com.sun.grizzly.tcp.http11.GrizzlyOutputBuffer;
-import com.sun.grizzly.tcp.http11.GrizzlyOutputStream;
 import com.sun.grizzly.tcp.http11.GrizzlyRequest;
 import com.sun.grizzly.tcp.http11.GrizzlyResponse;
-import com.sun.grizzly.util.buf.ByteChunk;
 import com.wesabe.bouncer.BackendService;
-import com.wesabe.bouncer.HttpHeaders;
 import com.wesabe.bouncer.ProxyAdapter;
+import com.wesabe.bouncer.http.ProxyRequest;
+import com.wesabe.bouncer.http.ProxyRequestFactory;
+import com.wesabe.bouncer.http.ProxyResponse;
+import com.wesabe.bouncer.http.ProxyResponseFactory;
 
 @RunWith(Enclosed.class)
 public class ProxyAdapterTest {
 	
-	public static class Proxying_A_GET_Request {
+	public static class Proxying_A_Request {
 		private BackendService backendService;
+		
 		private GrizzlyRequest request;
-		private GrizzlyOutputBuffer responseEntity;
 		@SuppressWarnings("unchecked")
 		private GrizzlyResponse response;
-		private HttpResponse proxyResponse;
+		
+		private ProxyResponse proxyResponse;
+		private ProxyRequest proxyRequest;
+		
+		private ProxyRequestFactory requestFactory;
+		private ProxyResponseFactory responseFactory;
+		
 		private ProxyAdapter adapter;
-		private StatusLine status;
-		private Header[] proxyResponseHeaders;
-		private HttpHeaders httpHeaders;
-		private HttpEntity proxyEntity;
-		@SuppressWarnings("unchecked")
-		private Response connectionResponse;
 		
 		@Before
 		public void setup() throws Exception {
-			this.httpHeaders = mock(HttpHeaders.class);
+			this.request = mock(GrizzlyRequest.class);
+			this.response = mock(GrizzlyResponse.class);
 			
-			this.proxyResponseHeaders = new Header[] {
-				new BasicHeader("Server", "Internal Sensitive"),
-				new BasicHeader("Content-Type", "application/json"),
-				new BasicHeader("X-Death-Face-McGee", "DO NOT SHOW THIS TO ANYONE"),
-			};
+			this.proxyRequest = mock(ProxyRequest.class);
+			this.proxyResponse = mock(ProxyResponse.class);
 			
-			this.status = mock(StatusLine.class);
-			when(status.getStatusCode()).thenReturn(301);
-			
-			this.proxyEntity = spy(new StringEntity("I am from the proxy."));
-			
-			this.proxyResponse = mock(HttpResponse.class);
-			when(proxyResponse.getStatusLine()).thenReturn(status);
-			when(proxyResponse.getAllHeaders()).thenReturn(proxyResponseHeaders);
-			when(proxyResponse.getEntity()).thenReturn(proxyEntity);
+			this.requestFactory = mock(ProxyRequestFactory.class);
+			when(requestFactory.buildFromGrizzlyRequest(request)).thenReturn(proxyRequest);
+			this.responseFactory = mock(ProxyResponseFactory.class);
 			
 			this.backendService = mock(BackendService.class);
-			when(backendService.execute(any(HttpUriRequest.class))).thenReturn(proxyResponse);
+			when(backendService.execute(proxyRequest)).thenReturn(proxyResponse);
 			
-			this.request = spy(new GrizzlyRequest());
-			Request connectionRequest = new Request();
-			connectionRequest.getMimeHeaders().setValue("Accept").setString("application/json");
-			connectionRequest.getMimeHeaders().setValue("X-Death-Face-McGee").setString("DO NOT SHOW THIS TO ANYONE");
-			request.setRequest(connectionRequest);
-			doReturn("1.2.3.4").when(request).getRemoteAddr();
-			doReturn("GET").when(request).getMethod();
-			doReturn("/hello").when(request).getRequestURI();
-			
-			
-			this.response = mock(GrizzlyResponse.class);
-			this.responseEntity = new GrizzlyOutputBuffer();
-			this.connectionResponse = mock(Response.class);
-			responseEntity.setResponse(connectionResponse);
-			when(response.getOutputStream()).thenReturn(new GrizzlyOutputStream(responseEntity));
-			
-			this.adapter = new ProxyAdapter(backendService, httpHeaders);
+			this.adapter = new ProxyAdapter(backendService, requestFactory, responseFactory);
 		}
 		
 		@Test
-		public void itRespondsWithTheEntityOfTheProxyResponse() throws Exception {
+		public void itBuildsAProxyRequestFromTheIncomingRequest() throws Exception {
 			adapter.service(request, response);
 			
-			InOrder inOrder = inOrder(connectionResponse);
-			inOrder.verify(connectionResponse).isCommitted();
-			inOrder.verify(connectionResponse).getContentLength();
-			inOrder.verify(connectionResponse).doWrite(argThat(anyObjWithToString("I am from the proxy.")));
-			inOrder.verify(connectionResponse).finish();
-			verifyNoMoreInteractions(connectionResponse);
+			verify(requestFactory).buildFromGrizzlyRequest(request);
 		}
 		
 		@Test
-		public void itRespondsWithANullEntityIfTheProxyResponseHasNoEntity() throws Exception {
-			doReturn(null).when(proxyResponse).getEntity();
-			
+		public void itSendsTheProxyRequestToTheBackendService() throws Exception {
 			adapter.service(request, response);
 			
-			verifyNoMoreInteractions(connectionResponse);
+			verify(backendService).execute(proxyRequest);
 		}
 		
 		@Test
-		public void itRespondsWithoutAnEntityIfTheProxyReponseHasNoEntity() throws Exception {
-			when(proxyResponse.getEntity()).thenReturn(null);
-			
+		public void itBuildsAnOutgoingResponseFromTheProxyResponse() throws Exception {
 			adapter.service(request, response);
 			
-			verify(proxyEntity, never()).getContent();
-		}
-
-		@Test
-		public void itSanitizesTheProxiedRequest() throws Exception {
-			when(httpHeaders.isValidRequestHeader("Accept")).thenReturn(true);
-			when(httpHeaders.isValidRequestHeader("X-Death-Face-McGee")).thenReturn(false);
-			
-			adapter.service(request, response);
-			
-			verify(backendService).execute(argThat(new ArgumentMatcher<HttpUriRequest>() {
-				@Override
-				public boolean matches(Object argument) {
-					HttpUriRequest request = (HttpUriRequest) argument;
-					try {
-						return request.getMethod().equals("GET") &&
-								request.getURI().equals(new URI("/hello")) &&
-								request.getFirstHeader("X-Death-Face-McGee") == null &&
-								request.getFirstHeader("Accept").getValue().equals("application/json") &&
-								request.getFirstHeader("X-Forwarded-For").getValue().equals("1.2.3.4")
-								;
-					} catch (URISyntaxException e) {
-						throw new RuntimeException(e);
-					}
-				}
-			}));
-		}
-		
-		@Test
-		public void itSanitizesTheProxiedResponse() throws Exception {
-			when(httpHeaders.isValidResponseHeader("Server")).thenReturn(true);
-			when(httpHeaders.isValidResponseHeader("Content-Type")).thenReturn(true);
-			when(httpHeaders.isValidResponseHeader("X-Death-Face-McGee")).thenReturn(false);
-			
-			adapter.service(request, response);
-			
-			InOrder inOrder = inOrder(response, httpHeaders);
-			inOrder.verify(response).setStatus(301);
-			inOrder.verify(httpHeaders).isValidResponseHeader("Server");
-			inOrder.verify(response).addHeader("Server", "Internal Sensitive");
-			inOrder.verify(httpHeaders).isValidResponseHeader("Content-Type");
-			inOrder.verify(response).addHeader("Content-Type", "application/json");
-			inOrder.verify(httpHeaders).isValidResponseHeader("X-Death-Face-McGee");
-			inOrder.verify(response, never()).setHeader("X-Death-Face-McGee", "DO NOT SHOW THIS TO ANYONE");
-			inOrder.verify(response).setHeader("Server", "Wesabe");
-			inOrder.verify(response).finishResponse();
-		}
-		
-		private ArgumentMatcher<ByteChunk> anyObjWithToString(final String string) {
-			return new ArgumentMatcher<ByteChunk>() {
-				@Override
-				public boolean matches(Object argument) {
-					return argument.toString().equals(string);
-				}
-			};
+			verify(responseFactory).buildFromHttpResponse(proxyResponse, response);
 		}
 	}
 }
