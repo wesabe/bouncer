2609292af18cf8498137ead2605dd83ec9fbb9d6
Coda Hale <coda.hale@gmail.com>
2009-03-10 19:44:22 -0700

    Added ProxyBackendService.

---
+++ src/main/java/com/wesabe/bouncer/ProxyBackendService.java
@@ -0,0 +1,44 @@
+package com.wesabe.bouncer;
+
+import java.net.URI;
+import java.util.logging.Logger;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+
+import com.wesabe.bouncer.http.HttpClientFactory;
+import com.wesabe.bouncer.http.ProxyRequest;
+
+/**
+ * A backend service which sends a {@link ProxyRequest} to a server (identified
+ * by a {@link URI}) and returns an {@link HttpResponse}.
+ * 
+ * @author coda
+ *
+ */
+public class ProxyBackendService implements BackendService {
+	private static final Logger LOGGER = Logger.getLogger(ProxyBackendService.class.getSimpleName());
+	private final URI uri;
+	private final HttpClient httpClient;
+	
+	public ProxyBackendService(URI uri, HttpClientFactory clientFactory) {
+		this.uri = uri;
+		this.httpClient = clientFactory.buildClient();
+	}
+	
+	@Override
+	public HttpResponse execute(ProxyRequest request) throws HttpException {
+		try {
+			LOGGER.info(String.format("%s %s", request.getMethod(), request.getURI().toString()));
+			return httpClient.execute(resolve(request));
+		} catch (Exception e) {
+			throw new HttpException("error executing request", e);
+		}
+	}
+	
+	private ProxyRequest resolve(ProxyRequest request) {
+		request.setURI(uri.resolve(request.getURI()));
+		return request;
+	}
+}
---
+++ src/test/java/com/wesabe/bouncer/tests/ProxyBackendServiceTest.java
@@ -0,0 +1,70 @@
+package com.wesabe.bouncer.tests;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+
+import com.wesabe.bouncer.ProxyBackendService;
+import com.wesabe.bouncer.http.HttpClientFactory;
+import com.wesabe.bouncer.http.ProxyRequest;
+
+@RunWith(Enclosed.class)
+public class ProxyBackendServiceTest {
+	public static class Initializing {
+		@Test
+		public void itBuildsAnHTTPClient() throws Exception {
+			HttpClientFactory factory = mock(HttpClientFactory.class);
+			
+			new ProxyBackendService(URI.create("http://0.0.0.0"), factory);
+			
+			verify(factory).buildClient();
+		}
+	}
+	
+	public static class Executing_An_HTTP_Request {
+		private HttpClientFactory factory;
+		private HttpClient client;
+		private ProxyBackendService service;
+		private ProxyRequest request;
+		private HttpResponse response;
+		
+		@Before
+		public void setup() throws Exception {
+			this.request = mock(ProxyRequest.class);
+			when(request.getURI()).thenReturn(URI.create("/woo"));
+			
+			this.response = mock(HttpResponse.class);
+			
+			this.client = mock(HttpClient.class);
+			when(client.execute(request)).thenReturn(response);
+			
+			this.factory = mock(HttpClientFactory.class);
+			when(factory.buildClient()).thenReturn(client);
+			
+			this.service = new ProxyBackendService(URI.create("http://0.0.0.0"), factory);
+		}
+		
+		@Test
+		public void itResolvesTheRequestURIForTheBackendURIBeforeExecution() throws Exception {
+			service.execute(request);
+			
+			InOrder inOrder = inOrder(client, request);
+			inOrder.verify(request).setURI(URI.create("http://0.0.0.0/woo"));
+			inOrder.verify(client).execute(request);
+		}
+		
+		@Test
+		public void itExecutesTheRequestAndReturnsTheResponse() throws Exception {
+			assertEquals(response, service.execute(request));
+		}
+	}
+}
