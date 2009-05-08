003322e3d9566efc72af3702422ab2649416778a
Coda Hale <coda@wesabe.com>
2009-05-07 23:39:30 -0700

    Added ProxyHttpExchange.

---
+++ src/main/java/com/wesabe/bouncer/proxy/ProxyHttpExchange.java
@@ -0,0 +1,206 @@
+package com.wesabe.bouncer.proxy;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mortbay.io.Buffer;
+import org.mortbay.jetty.client.HttpExchange;
+import org.mortbay.util.ajax.Continuation;
+
+import com.wesabe.servlet.normalizers.util.CaseInsensitiveSet;
+
+public class ProxyHttpExchange extends HttpExchange {
+	/**
+	 * Valid HTTP 1.1 general header fields.
+	 * 
+	 * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.5">RFC 2616 Section 4.5</a>
+	 */
+	protected static final Set<String> GENERAL_HEADERS = CaseInsensitiveSet.of(Locale.US,
+		"Cache-Control",
+		"Connection",
+		"Date",
+		"Pragma",
+		"Trailer",
+		"Transfer-Encoding",
+		"Upgrade",
+		"Via",
+		"Warning"
+	);
+	
+	/**
+	 * Valid HTTP 1.1 entity header fields.
+	 * 
+	 * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec7.html#sec7.1">RFC 2616, Section 7.1</a>
+	 */
+	protected static final Set<String> ENTITY_HEADERS = CaseInsensitiveSet.of(Locale.US,
+		"Allow",
+		"Content-Encoding",
+		"Content-Language",
+		"Content-Length",
+		"Content-Location",
+		"Content-MD5",
+		"Content-Range",
+		"Content-Type",
+		"Expires",
+		"Last-Modified"
+	);
+	
+	/**
+	 * Valid HTTP 1.1 request header fields.
+	 * 
+	 * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.3">RFC 2616, Section 5.3</a>
+	 */
+	protected static final Set<String>  REQUEST_HEADERS = CaseInsensitiveSet.of(Locale.US,
+		"Accept",
+		"Accept-Charset",
+		"Accept-Encoding",
+		"Accept-Language",
+		"Authorization",
+		"Expect",
+		"From",
+		"Host",
+		"If-Match",
+		"If-Modified-Since",
+		"If-None-Match",
+		"If-Range",
+		"If-Unmodified-Since",
+		"Max-Forwards",
+		"Proxy-Authorization",
+		"Range",
+		"Referer",
+		"TE",
+		"User-Agent"
+	);
+	
+	/**
+	 * Valid HTTP 1.1 response header fields.
+	 * 
+	 * @see <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.2">RFC 2616, Section 6.2</a>
+	 */
+	protected static final Set<String>  RESPONSE_HEADERS = CaseInsensitiveSet.of(Locale.US,
+		"Accept-Ranges",
+		"Age",
+		"ETag",
+		"Location",
+		"Proxy-Authenticate",
+		"Retry-After",
+		"Vary",
+		"WWW-Authenticate"
+	);
+	
+	/**
+	 * Unproxyable headers.
+	 */
+	protected static final Set<String>  UNPROXYABLE_HEADERS = CaseInsensitiveSet.of(Locale.US,
+		"Proxy-Connection",
+		"Connection",
+		"Keep-Alive",
+		"Transfer-Encoding",
+		"TE",
+		"Trailer",
+		"Proxy-Authorization",
+		"Proxy-Authenticate",
+		"Upgrade",
+		"Server"
+	);
+	
+	private final URI backendUri;
+	private final HttpServletRequest request;
+	private final HttpServletResponse response;
+	private final Continuation continuation;
+	
+	public ProxyHttpExchange(URI backend, HttpServletRequest request, HttpServletResponse response,
+			Continuation continuation) {
+		this.backendUri = backend;
+		this.request = request;
+		this.response = response;
+		this.continuation = continuation;
+		buildFromRequest(request);
+	}
+	
+	public URI getBackendUri() {
+		return backendUri;
+	}
+	
+	public HttpServletRequest getRequest() {
+		return request;
+	}
+	
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+	
+	private void buildFromRequest(HttpServletRequest request) {
+		addRequestHeader("Authorization", request.getUserPrincipal().toString());
+		setVersion(request.getProtocol());
+		setMethod(request.getMethod());
+		setURL(buildProxyUrl(request));
+
+		final Enumeration<?> names = request.getHeaderNames();
+		while (names.hasMoreElements()) {
+			String name = (String) names.nextElement();
+			if (!UNPROXYABLE_HEADERS.contains(name)
+					&& (GENERAL_HEADERS.contains(name)
+							|| ENTITY_HEADERS.contains(name)
+							|| REQUEST_HEADERS.contains(name))) {
+				final Enumeration<?> values = request.getHeaders(name);
+				while (values.hasMoreElements()) {
+					String value = (String) values.nextElement();
+					addRequestHeader(name, value);
+				}
+			}
+		}
+
+		if (request.getContentType() != null) {
+			try {
+				setRequestContentSource(request.getInputStream());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private String buildProxyUrl(HttpServletRequest request) {
+		final StringBuilder urlBuilder = new StringBuilder();
+		urlBuilder.append(request.getRequestURI());
+		final String queryString = request.getQueryString();
+		if (queryString != null) {
+			urlBuilder.append('?').append(queryString);
+		}
+		return backendUri.resolve(urlBuilder.toString()).toASCIIString();
+	}
+
+	@Override
+	protected void onResponseComplete() throws IOException {
+		continuation.resume();
+	}
+
+	@Override
+	protected void onResponseContent(Buffer content) throws IOException {
+		content.writeTo(response.getOutputStream());
+	}
+
+	@Override
+	protected void onResponseStatus(Buffer version, int status, Buffer reason) throws IOException {
+		response.setStatus(status);
+	}
+
+	@Override
+	protected void onResponseHeader(Buffer nameBuffer, Buffer valueBuffer) throws IOException {
+		final String name = nameBuffer.toString();
+		if (!UNPROXYABLE_HEADERS.contains(name)
+				&& (GENERAL_HEADERS.contains(name)
+						|| ENTITY_HEADERS.contains(name)
+						|| RESPONSE_HEADERS.contains(name))) {
+			if (valueBuffer != null) {
+				response.addHeader(name, valueBuffer.toString());
+			}
+		}
+	}
+}
---
+++ src/test/java/com/wesabe/bouncer/proxy/tests/ProxyHttpExchangeTest.java
@@ -0,0 +1,270 @@
+package com.wesabe.bouncer.proxy.tests;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.mortbay.io.Buffer;
+import org.mortbay.io.ByteArrayBuffer;
+import org.mortbay.util.ajax.Continuation;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.wesabe.bouncer.auth.WesabeCredentials;
+import com.wesabe.bouncer.proxy.ProxyHttpExchange;
+
+@RunWith(Enclosed.class)
+public class ProxyHttpExchangeTest {
+	public static class Proxying_A_Request {
+		private URI backend;
+		private HttpServletRequest request;
+		private HttpServletResponse response;
+		private Continuation continuation;
+		private ServletInputStream inputStream;
+		private WesabeCredentials principal;
+		private final Map<String, String> requestHeaders = ImmutableMap.of(
+			"Date", "Wed, 15 Nov 1995 06:25:24 GMT",
+			"Accept", "application/json",
+			"Last-Modified", "Wed, 15 Nov 1995 06:25:25 GMT",
+			"Keep-Alive", "true",
+			"X-Boggle", "Ninja!"
+		);
+		
+		@Before
+		public void setup() throws Exception {
+			this.principal = new WesabeCredentials(300, "WOOHOO");
+			
+			this.backend = URI.create("http://example.com:8081/");
+			
+			this.inputStream = mock(ServletInputStream.class);
+			
+			this.request = mock(HttpServletRequest.class);
+			when(request.getProtocol()).thenReturn("HTTP/1.0");
+			when(request.getMethod()).thenReturn("POST");
+			when(request.getRequestURI()).thenReturn("/dingofroop");
+			when(request.getHeaderNames()).thenReturn(Collections.enumeration(requestHeaders.keySet()));
+			when(request.getHeaders(anyString())).thenAnswer(new Answer<Enumeration<?>> () {
+				@Override
+				public Enumeration<?> answer(InvocationOnMock invocation) throws Throwable {
+					final String name = (String) invocation.getArguments()[0];
+					return Collections.enumeration(ImmutableList.of(requestHeaders.get(name)));
+				}
+			});
+			when(request.getInputStream()).thenReturn(inputStream);
+			when(request.getUserPrincipal()).thenReturn(principal);
+			
+			this.response = mock(HttpServletResponse.class);
+			this.continuation = mock(Continuation.class);
+		}
+		
+		private ProxyHttpExchange exchange() {
+			return new ProxyHttpExchange(backend, request, response, continuation);
+		}
+		
+		@Test
+		public void itHasABackendUri() throws Exception {
+			assertThat(exchange().getBackendUri(), is(backend));
+		}
+		
+		@Test
+		public void itHasARequest() throws Exception {
+			assertThat(exchange().getRequest(), is(request));
+		}
+		
+		@Test
+		public void itHasAResponse() throws Exception {
+			assertThat(exchange().getResponse(), is(response));
+		}
+		
+		@Test
+		public void itSetsTheWesabeAuthorizationHeader() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("Authorization"), is("Wesabe MzAwOldPT0hPTw=="));
+		}
+		
+		@Test
+		public void itCopiesTheRequestsProtocolVersion() throws Exception {
+			assertThat(exchange().getVersion(), is(10));
+			
+			verify(request).getProtocol();
+		}
+		
+		@Test
+		public void itCopiesTheRequestsMethod() throws Exception {
+			assertThat(exchange().getMethod(), is("POST"));
+		}
+		
+		@Test
+		public void itCopiesTheRequestsURI() throws Exception {
+			assertThat(exchange().getURI(), is("/dingofroop"));
+		}
+		
+		@Test
+		public void itCopiesTheRequestsQueryString() throws Exception {
+			when(request.getQueryString()).thenReturn("q=1");
+			assertThat(exchange().getURI(), is("/dingofroop?q=1"));
+		}
+		
+		@Test
+		public void itSetsTheBackendsHost() throws Exception {
+			assertThat(exchange().getAddress().getHost(), is("example.com"));
+		}
+		
+		@Test
+		public void itSetsTheBackendsPort() throws Exception {
+			assertThat(exchange().getAddress().getPort(), is(8081));
+		}
+		
+		@Test
+		public void itCopiesGeneralHttpHeadersToTheRequest() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("Date"), is("Wed, 15 Nov 1995 06:25:24 GMT"));
+		}
+		
+		@Test
+		public void itCopiesRequestHttpHeadersToTheRequest() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("Accept"), is("application/json"));
+		}
+		
+		@Test
+		public void itCopiesEntityHttpHeadersToTheRequest() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("Last-Modified"), is("Wed, 15 Nov 1995 06:25:25 GMT"));
+		}
+		
+		@Test
+		public void itDoesNotCopyUnproxyableHttpHeadersToTheRequest() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("Keep-Alive"), is(nullValue()));
+		}
+		
+		@Test
+		public void itDoesNotCopyNonstandardHttpHeadersToTheRequest() throws Exception {
+			assertThat(exchange().getRequestFields().getStringField("X-Boggle"), is(nullValue()));
+		}
+		
+		@Test
+		public void itCopiesTheInputStreamIfThereIsAContentType() throws Exception {
+			when(request.getContentType()).thenReturn("application/json");
+			
+			assertThat((ServletInputStream) exchange().getRequestContentSource(), is(inputStream));
+		}
+		
+		@Test
+		public void itDoesNotCopyTheInputStreamIfThereIsntAContentType() throws Exception {
+			when(request.getContentType()).thenReturn(null);
+			
+			assertThat(exchange().getRequestContentSource(), is(nullValue()));
+		}
+		
+		@Test
+		public void itBubblesUpAnyErrorCopyingTheInputStream() throws Exception {
+			when(request.getContentType()).thenReturn("application/json");
+			when(request.getInputStream()).thenThrow(new IOException("THE BEES THEY'RE IN MY EYES"));
+			
+			try {
+				exchange();
+				fail("should have thrown a RuntimeException but didn't");
+			} catch (RuntimeException e) {
+				assertThat(e.getCause(), is(IOException.class));
+			}
+		}
+	}
+	
+	public static class Proxying_A_Response {
+		private URI backend;
+		private HttpServletRequest request;
+		private HttpServletResponse response;
+		private Continuation continuation;
+		private ServletOutputStream outputStream;
+		private ProxyHttpExchange exchange;
+		
+		@Before
+		public void setup() throws Exception {
+			this.backend = URI.create("http://example.com:8081/");
+			this.request = mock(HttpServletRequest.class);
+			when(request.getProtocol()).thenReturn("HTTP/1.0");
+			when(request.getMethod()).thenReturn("POST");
+			when(request.getRequestURI()).thenReturn("/dingofroop");
+			when(request.getHeaderNames()).thenReturn(Collections.enumeration(ImmutableList.of()));
+			when(request.getUserPrincipal()).thenReturn(new WesabeCredentials(200, "WOO"));
+			this.outputStream = mock(ServletOutputStream.class);
+			this.response = mock(HttpServletResponse.class);
+			when(response.getOutputStream()).thenReturn(outputStream);
+			this.continuation = mock(Continuation.class);
+			this.exchange = new ProxyHttpExchange(backend, request, response, continuation);
+		}
+		
+		@Test
+		public void itCopiesTheResponseEntity() throws Exception {
+			final Buffer content = mock(Buffer.class);
+			
+			exchange.getEventListener().onResponseContent(content);
+			
+			verify(content).writeTo(outputStream);
+		}
+		
+		@Test
+		public void itCopiesTheResponseStatus() throws Exception {
+			exchange.getEventListener().onResponseStatus(null, 302, null);
+			
+			verify(response).setStatus(302);
+		}
+		
+		@Test
+		public void itCopiesGeneralHeadersFromTheResponse() throws Exception {
+			exchange.getEventListener().onResponseHeader(new ByteArrayBuffer("Date"), new ByteArrayBuffer("blah"));
+			
+			verify(response).addHeader("Date", "blah");
+		}
+		
+		@Test
+		public void itCopiesEntityHeadersFromTheResponse() throws Exception {
+			exchange.getEventListener().onResponseHeader(new ByteArrayBuffer("Content-Type"), new ByteArrayBuffer("application/json"));
+			
+			verify(response).addHeader("Content-Type", "application/json");
+		}
+		
+		@Test
+		public void itCopiesResponseHeadersFromTheResponse() throws Exception {
+			exchange.getEventListener().onResponseHeader(new ByteArrayBuffer("Age"), new ByteArrayBuffer("viejo"));
+			
+			verify(response).addHeader("Age", "viejo");
+		}
+		
+		@Test
+		public void itDoesNotCopyUnproxyableHeadersFromTheResponse() throws Exception {
+			exchange.getEventListener().onResponseHeader(new ByteArrayBuffer("Server"), new ByteArrayBuffer("internal"));
+			
+			verify(response, never()).addHeader("Server", "internal");
+		}
+		
+		@Test
+		public void itDoesNotCopyHeadersWithNullValuesFromTheResponse() throws Exception {
+			exchange.getEventListener().onResponseHeader(new ByteArrayBuffer("Age"), null);
+			
+			verify(response, never()).addHeader("Age", null);
+		}
+		
+		@Test
+		public void itResumesTheContinuationWhenTheResponseIsComplete() throws Exception {
+			exchange.getEventListener().onResponseComplete();
+			
+			verify(continuation).resume();
+		}
+	}
+}
